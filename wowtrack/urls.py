c55a18a7a464165b351f2a71d8490c5321fb1f2b
Matt Hunzinger <matthunz2@gmail.com>
2016-12-24 15:30:57 -0500

    Create home page

--- wowtrack/urls.py
+++ wowtrack/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls', namespace='accounts')),
-    url(r'^characters/', include('characters.urls', namespace='characters'))
+    url(r'^characters/', include('characters.urls', namespace='characters')),
+    url(r'', views.HomeView.as_view()),
 ]
