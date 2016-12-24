154d7542feb6d647ce707dda1ca7dfb159587fda
Matt Hunzinger <matthunz2@gmail.com>
2016-12-24 15:18:29 -0500

    Create login page

--- wowtrack/urls.py
+++ wowtrack/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^characters/', include('characters.urls', namespace='characters'))
 ]
