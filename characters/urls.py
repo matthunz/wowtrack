e0b4223e673fc55b20576e32ef7c6019a9f80d67
Matt Hunzinger <matthunz2@gmail.com>
2016-12-31 00:34:55 -0500

    Add character creation page

--- characters/urls.py
+++ characters/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^new/', views.CharacterCreateView.as_view(), name='create'),
     url(r'(?P<realm>[\w\-]+)/(?P<name>[\w\-]+)', views.CharacterDetailView.as_view(), name='detail'),
 ]
--- characters/views.py
+++ characters/views.py
@@ -1,6 +1,8 @@
+from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404
 from django.views import generic
+from django.shortcuts import get_object_or_404
 from . import models
 import requests
 
@@ -9,22 +11,22 @@ class CharacterDetailView(LoginRequiredMixin, generic.DetailView):
     model = models.Character
 
     def get_object(self, queryset=None):
-        try:
-            return self.model.objects.get(name__iexact=self.kwargs['name'], realm__iexact=self.kwargs['realm'])
-        except self.model.DoesNotExist:
-            request = requests.get(
-                'https://us.api.battle.net/wow/character/%s/%s?locale=en_US&fields=items&apikey=%s' % (
-                    self.kwargs['realm'], self.kwargs['name'], 'x95frpjm3v3zdrnj3rbzr83e9b348hj9'
-                ))
+        return get_object_or_404(self.model, name__iexact=self.kwargs['name'], realm__iexact=self.kwargs['realm'])
 
-            if request.status_code != 200:
-                raise Http404
 
-            json = request.json()
-            character = self.model.objects.create(
-                name=self.kwargs['name'],
-                realm=self.kwargs['realm']
-            )
-            models.Snapshot.objects.create(character=character, item_level=json['items']['averageItemLevel'])
+class CharacterCreateView(LoginRequiredMixin, generic.CreateView):
+    model = models.Character
+    fields = ['name', 'realm']
+    success_url = reverse_lazy('accounts:profile')
 
-            return character
+    def form_valid(self, form):
+        request = requests.get(
+            'https://us.api.battle.net/wow/character/%s/%s?locale=en_US&fields=items&apikey=%s' % (
+                form.instance.realm, form.instance.name, 'x95frpjm3v3zdrnj3rbzr83e9b348hj9'
+            ))
+
+        if request.status_code != 200:
+            raise Http404
+
+        form.instance.user = self.request.user
+        return super(CharacterCreateView, self).form_valid(form)
